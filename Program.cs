58d9ebfc36e4b06cab8579162523c587bf5690c6
Joel Caples <jmcaples@gmail.com>
2020-06-05 17:11:46 -0500

    new project

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace login_tasks {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            try {
+                Console.Write("Pwd: ");
+                var value = Console.ReadLine();
+
+                var commands = new List<string>() {
+                    $@"""C:\Program Files\VeraCrypt\VeraCrypt.exe"" /q /v \Device\Harddisk0\Partition2 /l V /password {value}",
+                    $@"""C:\Program Files\VeraCrypt\VeraCrypt.exe"" /q /v \Device\Harddisk2\Partition1 /l W /password {value}"
+                };
+
+                foreach (var cmd in commands) {
+                    System.Diagnostics.Process.Start("CMD.exe", $@"/C {cmd}");
+                }
+            }
+            catch (Exception) { }
+        }
+    }
+}
