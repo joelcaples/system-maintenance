ede644809bbe42b47d9fb4198930807d626f9406
Joel Caples <jmcaples@gmail.com>
2020-06-05 19:59:17 -0500

    Refactored.  Added the SyncToy step

---
+++ Models/RunTask.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace login_tasks.Models {
+  class RunTask {
+    public string FilePath { get; set; }
+    public string Arguments { get; set; }
+        public string TaskName { get; set; }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using login_tasks.Models;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace login_tasks {
 
@@ -8,19 +10,41 @@ namespace login_tasks {
         static void Main(string[] args) {
 
             try {
-                Console.Write("Pwd: ");
+
                 var value = ReadLine.ReadPassword("Pwd:");
 
-                var commands = new List<string>() {
-                    $@"""C:\Program Files\VeraCrypt\VeraCrypt.exe"" /q /v \Device\Harddisk0\Partition2 /l V /password {value}",
-                    $@"""C:\Program Files\VeraCrypt\VeraCrypt.exe"" /q /v \Device\Harddisk2\Partition1 /l W /password {value}"
+                var commands = new List<RunTask>() {
+                    //Mount main drive
+                    new RunTask() {FilePath=$@"""C:\Program Files\VeraCrypt\VeraCrypt.exe""", Arguments=$@"/q /v \Device\Harddisk0\Partition2 /l V /password {value}", TaskName="Mount Main Drive" },
+                    //Mount Backup drive
+                    new RunTask() {FilePath=$@"""C:\Program Files\VeraCrypt\VeraCrypt.exe""", Arguments=$@"/q /v \Device\Harddisk2\Partition1 /l W /password {value}", TaskName="Mount Backup Drive" },
+                    //Create synctoy pair
+                    new RunTask() {FilePath=$@"""C:\Program Files\SyncToy 2.1\Synctoy.exe""", Arguments=$@"-d(left=V:\,right=W:\bu-current\,name=bu_local_sd,operation=echo)", TaskName="Configure Backup Job" },
+                    //Use synctoy to backup the SD card to the mounted volume
+                    new RunTask() {FilePath=$@"""C:\Program Files\SyncToy 2.1\Synctoycmd.exe""", Arguments=$@"-R bu_local_sd", TaskName="Backup"  },
+                     //Remove synctoy pair
+                    new RunTask() {FilePath=$@"""C:\Program Files\SyncToy 2.1\Synctoy.exe""", Arguments=$@"-ubu_local_sd", TaskName="Teardown Backup Job"  }
                 };
 
                 foreach (var cmd in commands) {
-                    System.Diagnostics.Process.Start("CMD.exe", $@"/C {cmd}");
+                    var process = new Process() {
+                        StartInfo = new ProcessStartInfo() {
+                            FileName = cmd.FilePath,
+                            Arguments = cmd.Arguments
+                        }
+                    };
+                    try {
+                        Console.WriteLine($@"Running {cmd.TaskName}...");
+                        process.Start();
+                        process.WaitForExit();
+                    } catch (Exception e) {
+                        Console.WriteLine($@"{process.StartInfo.FileName} {process.StartInfo.Arguments}");
+                        throw (e);
+                    }
                 }
+            } catch (Exception e) {
+                Console.WriteLine(e.Message);
             }
-            catch (Exception) { }
         }
     }
 }
